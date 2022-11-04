9649ec47dd3c1236146de9387b161d90ba14bac2
Mayara Nascimento <mayara.dnascimentos@gmail.com>
2022-11-04 01:41:20 -0300

    create object with world cup informations

---
+++ games.js
@@ -0,0 +1,321 @@
+const json = [
+  {
+    date: "20/11",
+    day: "domingo",
+    games: [
+      {
+        player1: "qatar",
+        hour: "13:00",
+        player2: "ecuador",
+      },
+    ],
+  },
+  {
+    date: "21/11",
+    day: "segunda-feira",
+    games: [
+      {
+        player1: "england",
+        hour: "10:00",
+        player2: "iran",
+      },
+      {
+        player1: "senegal",
+        hour: "13:00",
+        player2: "netherlands",
+      },
+      {
+        player1: "united_states",
+        hour: "16:00",
+        player2: "wales",
+      },
+    ],
+  },
+  {
+    date: "22/11/2022",
+    day: "terça-feira",
+    games: [
+      {
+        player1: "argentina",
+        hour: "07:00",
+        player2: "saudi_arabia",
+      },
+      {
+        player1: "denmark",
+        hour: "10:00",
+        player2: "tunisia",
+      },
+      {
+        player1: "mexico",
+        hour: "13:00",
+        player2: "poland",
+      },
+      {
+        player1: "france",
+        hour: "16:00",
+        player2: "australia",
+      },
+    ],
+  },
+  {
+    date: "23/11/2022",
+    day: "quarta-feira",
+    games: [
+      {
+        player1: "marocco",
+        hour: "07:00",
+        player2: "croatia",
+      },
+      {
+        player1: "germany",
+        hour: "10:00",
+        player2: "japan",
+      },
+      {
+        player1: "spain",
+        hour: "13:00",
+        player2: "costa_rica",
+      },
+      {
+        player1: "belgium",
+        hour: "16:00",
+        player2: "canada",
+      },
+    ],
+  },
+  {
+    date: "24/11/2022",
+    day: "quinta-feira",
+    games: [
+      {
+        player1: "switzerland",
+        hour: "07:00",
+        player2: "cameroon",
+      },
+      {
+        player1: "uruguay",
+        hour: "10:00",
+        player2: "korea_republic",
+      },
+      {
+        player1: "portugal",
+        hour: "13:00",
+        player2: "ghana",
+      },
+      {
+        player1: "brazil",
+        hour: "16:00",
+        player2: "serbia",
+      },
+    ],
+  },
+  {
+    date: "25/11/2022",
+    day: "sexta-feira",
+    games: [
+      {
+        player1: "wales",
+        hour: "07:00",
+        player2: "iran",
+      },
+      {
+        player1: "qatar",
+        hour: "10:00",
+        player2: "senegal",
+      },
+      {
+        player1: "netherlands",
+        hour: "13:00",
+        player2: "ecuador",
+      },
+      {
+        player1: "england",
+        hour: "16:00",
+        player2: "united_states",
+      },
+    ],
+  },
+  {
+    date: "26/11/2022",
+    day: "sábado",
+    games: [
+      {
+        player1: "tunisia",
+        hour: "07:00",
+        player2: "australia",
+      },
+      {
+        player1: "poland",
+        hour: "10:00",
+        player2: "saudi_arabia",
+      },
+      {
+        player1: "france",
+        hour: "13:00",
+        player2: "denmark",
+      },
+      {
+        player1: "argentina",
+        hour: "16:00",
+        player2: "mexico",
+      },
+    ],
+  },
+  {
+    date: "27/11/2022",
+    day: "domingo",
+    games: [
+      {
+        player1: "japan",
+        hour: "07:00",
+        player2: "costa_rica",
+      },
+      {
+        player1: "belgium",
+        hour: "10:00",
+        player2: "marocco",
+      },
+      {
+        player1: "croatia",
+        hour: "13:00",
+        player2: "canada",
+      },
+      {
+        player1: "spain",
+        hour: "16:00",
+        player2: "germany",
+      },
+    ],
+  },
+  {
+    date: "28/11/2022",
+    day: "segunda-feira",
+    games: [
+      {
+        player1: "cameroon",
+        hour: "07:00",
+        player2: "serbia",
+      },
+      {
+        player1: "korea_republic",
+        hour: "10:00",
+        player2: "ghana",
+      },
+      {
+        player1: "brazil",
+        hour: "13:00",
+        player2: "switzerland",
+      },
+      {
+        player1: "portugal",
+        hour: "16:00",
+        player2: "uruguay",
+      },
+    ],
+  },
+  {
+    date: "29/11/2022",
+    day: "terça-feira",
+    games: [
+      {
+        player1: "ecuador",
+        hour: "12:00",
+        player2: "senegal",
+      },
+      {
+        player1: "netherlands",
+        hour: "12:00",
+        player2: "qatar",
+      },
+      {
+        player1: "iran",
+        hour: "16:00",
+        player2: "united_states",
+      },
+      {
+        player1: "wales",
+        hour: "16:00",
+        player2: "england",
+      },
+    ],
+  },
+  {
+    date: "30/11/2022",
+    day: "quarta-feira",
+    games: [
+      {
+        player1: "tunisia",
+        hour: "12:00",
+        player2: "france",
+      },
+      {
+        player1: "australia",
+        hour: "12:00",
+        player2: "denmark",
+      },
+      {
+        player1: "poland",
+        hour: "16:00",
+        player2: "argentina",
+      },
+      {
+        player1: "saudi_arabia",
+        hour: "16:00",
+        player2: "mexico",
+      },
+    ],
+  },
+  {
+    date: "01/12/2022",
+    day: "quinta-feira",
+    games: [
+      {
+        player1: "croatia",
+        hour: "12:00",
+        player2: "belgium",
+      },
+      {
+        player1: "canada",
+        hour: "12:00",
+        player2: "marocco",
+      },
+      {
+        player1: "japan",
+        hour: "16:00",
+        player2: "spain",
+      },
+      {
+        player1: "costa_rica",
+        hour: "16:00",
+        player2: "germany",
+      },
+    ],
+  },
+  {
+    date: "02/12/2022",
+    day: "sexta-feira",
+    games: [
+      {
+        player1: "korea_republic",
+        hour: "12:00",
+        player2: "portugal",
+      },
+      {
+        player1: "ghana",
+        hour: "12:00",
+        player2: "uruguay",
+      },
+      {
+        player1: "serbia",
+        hour: "16:00",
+        player2: "switzerland",
+      },
+      {
+        player1: "cameroon",
+        hour: "16:00",
+        player2: "brazil",
+      },
+    ],
+  },
+]
+export default json
